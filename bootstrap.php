0b3677efd58cd9edd4fb721a09456b1335af388c
Ma-Sousa <ma.sousa2054@gmail.com>
2026-02-12 13:21:28 -0300

    chora: update gitignore and bootstrap setup

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,11 @@ require_once __DIR__ . "/csrf.php";
 require_once __DIR__ . "/src/CustomerRepository.php";
 require_once __DIR__ . "/src/Database.php";
 
+use App\Database;
+use App\CustomerRepository;
+
 $config = require __DIR__ . "/config.php";
+
 $db = new Database($config);
 $pdo = $db->pdo();
 $customersRepo = new CustomerRepository($pdo);
